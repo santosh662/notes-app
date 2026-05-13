a3da7195f92dffc3851a08fa69c35862aaa92a6e
MR_SINGH <65212069+santosh662@users.noreply.github.com>
2026-05-13 23:23:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Satyam - Notes App</title>
+    <title>Santosh - Notes App</title>
     <style>
         body {
             font-family: "Segoe UI", Arial, sans-serif;
