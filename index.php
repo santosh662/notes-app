c0aebf10d4064d5dd88b43e52aa9eec2fee307e5
MR_SINGH <65212069+santosh662@users.noreply.github.com>
2026-05-13 23:26:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Santosh - Notes App</title>
+    <title>Satyam- Notes App</title>
     <style>
         body {
             font-family: "Segoe UI", Arial, sans-serif;
@@ -124,7 +124,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>MR.SINGH- Notes App</h1>
+        <h1> Qualiybytes- Notes App</h1>
         <form action="save.php" method="post">
             <textarea name="note" placeholder="Write your notes here..."></textarea><br>
             <button type="submit">💾 Save Note</button>
